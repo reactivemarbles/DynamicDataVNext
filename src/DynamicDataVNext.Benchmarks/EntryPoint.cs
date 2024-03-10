5886d09235e766e17525a5a1967f8dbad1668c7a
JakenVeina <jakenveina@gmail.com>
2024-03-10 01:11:19 -0600

    Created skeleton solution

---
+++ src/DynamicDataVNext.Benchmarks/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace DynamicDataVNext.Benchmarks;
+
+public static class EntryPoint
+{
+    public static void Main(string[] args)
+        => BenchmarkSwitcher
+            .FromAssembly(Assembly.GetExecutingAssembly())
+            .Run(args, DefaultConfig.Instance
+                .WithArtifactsPath(Path.Combine(
+                    GetProjectRootDirectory(),
+                    Path.GetFileName(DefaultConfig.Instance.ArtifactsPath))));
+
+    // Cheesy way to get the project path, by getting the compiler to inject it.
+    private static string GetProjectRootDirectory([CallerFilePath] string? callerFilePath = null)
+        => Path.GetDirectoryName(callerFilePath)!;
+}
