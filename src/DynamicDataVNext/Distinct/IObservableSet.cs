089f332bd4f36a24c2a2f42304166a408e12c9b0
JakenVeina <jakenveina@gmail.com>
2024-03-21 23:52:25 -0500

    Adjusted collection names, after discussion

--- src/DynamicDataVNext/Distinct/IObservableSet.cs
+++ src/DynamicDataVNext/Distinct/IObservableSet.cs
@@ -5,31 +5,13 @@ using System.Reactive;
 namespace DynamicDataVNext;
 
 /// <summary>
-/// Describes a collection of distinct items, which publishes notifications about its mutations to subscribers, as they occur.
+/// Describes a collection of distinct items, which may not be mutated by the consumer, and which publishes notifications about its mutations to subscribers, as they occur.
 /// </summary>
 /// <typeparam name="T">The type of the items in the collection.</typeparam>
 public interface IObservableSet<T>
-    : ISet<T>,
+    : IReadOnlySet<T>,
         IObservable<DistinctChangeSet<T>>
 {
-    /// <summary>
-    /// An event that occurs after any mutation of the collection occurs.
-    /// </summary>
+    /// <inheritdoc cref="ISubjectSet{T}.CollectionChanged"/>
     IObservable<Unit> CollectionChanged { get; }
-
-    /// <summary>
-    /// Resets the items within the collection to the given set of items. I.E. clears and then re-populates the collection, as a single operation.
-    /// </summary>
-    /// <param name="items">The set of items with which to populate the collection.</param>
-    /// <exception cref="ArgumentNullException">Throws for <paramref name="items"/>.</exception>
-    void Reset(IEnumerable<T> items);
-
-    /// <summary>
-    /// Temporarily suspends the publication of notifications by the collection, until the returned object is disposed, at which point all mutations made during the suspension will (if any) will be published as one notification.
-    /// </summary>
-    /// <returns>An object that will trigger the resumption of notifications, when disposed.</returns>
-    /// <remarks>
-    /// May be called multiple times, in which case no notifications will be published until all outstanding suspensions have been disposed.
-    /// </remarks>
-    IDisposable SuspendNotifications();
 }
---
+++ src/DynamicDataVNext/Distinct/ISubjectSet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+
+namespace DynamicDataVNext;
+
+/// <summary>
+/// Describes a collection of distinct items, which publishes notifications about its mutations to subscribers, as they occur.
+/// </summary>
+/// <typeparam name="T">The type of the items in the collection.</typeparam>
+public interface ISubjectSet<T>
+    : ISet<T>,
+        IObservable<DistinctChangeSet<T>>
+{
+    /// <summary>
+    /// An event that occurs after any mutation of the collection occurs.
+    /// </summary>
+    IObservable<Unit> CollectionChanged { get; }
+
+    /// <summary>
+    /// Resets the items within the collection to the given set of items. I.E. clears and then re-populates the collection, as a single operation.
+    /// </summary>
+    /// <param name="items">The set of items with which to populate the collection.</param>
+    /// <exception cref="ArgumentNullException">Throws for <paramref name="items"/>.</exception>
+    void Reset(IEnumerable<T> items);
+
+    /// <summary>
+    /// Temporarily suspends the publication of notifications by the collection, until the returned object is disposed, at which point all mutations made during the suspension will (if any) will be published as one notification.
+    /// </summary>
+    /// <returns>An object that will trigger the resumption of notifications, when disposed.</returns>
+    /// <remarks>
+    /// May be called multiple times, in which case no notifications will be published until all outstanding suspensions have been disposed.
+    /// </remarks>
+    IDisposable SuspendNotifications();
+}
